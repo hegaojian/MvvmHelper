bf8c600d634357529652bf906479d0652fea724a
hegaojian <hegaojian123>
2021-11-02 11:37:08 +0800

    fix demo 上传下载bug

--- app/src/main/java/com/zhixinhuixue/zsyte/xxx/ui/viewmodel/TestViewModel.kt
+++ app/src/main/java/com/zhixinhuixue/zsyte/xxx/ui/viewmodel/TestViewModel.kt
@@ -1,18 +1,20 @@
 package com.zhixinhuixue.zsyte.xxx.ui.viewmodel
 
 import android.net.Uri
+import android.os.Build
 import androidx.lifecycle.rxLifeScope
 import com.zhixinhuixue.zsyte.xxx.app.api.NetUrl
 import com.zhixinhuixue.zsyte.xxx.app.util.Android10DownloadFactory
 import kotlinx.coroutines.Dispatchers
 import me.hgj.mvvmhelper.base.BaseViewModel
 import me.hgj.mvvmhelper.base.appContext
-import rxhttp.awaitString
 import rxhttp.toDownload
 import rxhttp.wrapper.entity.Progress
 import rxhttp.wrapper.entity.ProgressT
 import rxhttp.wrapper.param.RxHttp
+import rxhttp.wrapper.param.toResponse
 import rxhttp.wrapper.param.upload
+import java.io.File
 
 /**
  * 作者　: hegaojian
@@ -27,16 +29,28 @@ class TestViewModel : BaseViewModel() {
      * @param downLoadSuccess Function1<String, Unit>
      * @param downLoadError Function1<Throwable, Unit>
      */
-    fun downLoad(downLoadData:(ProgressT<Uri>)->Unit = {}, downLoadSuccess:(String)->Unit, downLoadError:(Throwable)->Unit = {}){
+    fun downLoad(downLoadData:(ProgressT<Any>)->Unit = {}, downLoadSuccess:(String)->Unit, downLoadError:(Throwable)->Unit = {}){
         rxLifeScope.launch({
-            val factory = Android10DownloadFactory(appContext,"${System.currentTimeMillis()}.apk")
-            val downLoadPath = RxHttp.get(NetUrl.DOWNLOAD_URL).toDownload(factory,
-                Dispatchers.Main) { progressT: ProgressT<Uri> ->
-                //下载中回调
-                downLoadData.invoke(progressT)
-            }.await()
-            //下载完成 回调
-            downLoadSuccess.invoke(downLoadPath.toString())
+            if(checkedAndroid_Q()){
+                //android 10
+                val factory = Android10DownloadFactory(appContext,"${System.currentTimeMillis()}.apk")
+                val downLoadPath = RxHttp.get(NetUrl.DOWNLOAD_URL).toDownload(factory,
+                    Dispatchers.Main) { progressT: ProgressT<Uri> ->
+                    //下载中回调
+                    downLoadData.invoke(progressT as ProgressT<Any>)
+                }.await()
+                //下载完成 回调
+                downLoadSuccess.invoke(downLoadPath.toString())
+            }else{
+                //10以下
+                val downLoadPath = RxHttp.get(NetUrl.DOWNLOAD_URL)
+                    .toDownload(appContext.externalCacheDir!!.absolutePath+"/${System.currentTimeMillis()}.apk",Dispatchers.Main) {
+                        downLoadData.invoke(it as ProgressT<Any>)
+                    }
+                    .await()
+                //下载完成 回调
+                downLoadSuccess.invoke(downLoadPath)
+            }
         },{
             //下载失败回调
             downLoadError.invoke(it)
@@ -50,16 +64,33 @@ class TestViewModel : BaseViewModel() {
      * @param uploadError Function1<Throwable, Unit>
      */
     fun upload(filePath:String, uploadData:(Progress)->Unit = {}, uploadSuccess:(String)->Unit, uploadError:(Throwable)->Unit = {}){
-        rxLifeScope.launch({
-            val result = RxHttp.postForm(NetUrl.UPLOAD_URL)
-                .addPart(me.hgj.mvvmhelper.base.appContext, "apkFile", Uri.parse(filePath))
-                .upload(this) {
-                    //上传进度回调
-                    uploadData.invoke(it)
-                }.awaitString()
-            uploadSuccess.invoke(result)
-        }, {
-            uploadError.invoke(it)
-        })
+         rxLifeScope.launch({
+             if (checkedAndroid_Q() && filePath.startsWith("content:")) {
+                 //android 10
+                 val result = RxHttp.postForm(NetUrl.UPLOAD_URL)
+                     .addPart(appContext, "apkFile", Uri.parse(filePath))
+                     .upload(this) {
+                         //上传进度回调
+                         uploadData.invoke(it)
+                     }.toResponse<String>().await()
+                 uploadSuccess.invoke(result)
+             } else {
+                 //10以下
+                 val result = RxHttp.postForm(NetUrl.UPLOAD_URL)
+                     .addFile("apkFile", File(filePath))
+                     .upload(this) {
+                         //上传进度回调
+                         uploadData.invoke(it)
+                     }.toResponse<String>().await()
+                 uploadSuccess.invoke(result)
+             }
+
+         }, {
+             uploadError.invoke(it)
+         })
+    }
+
+    private fun checkedAndroid_Q(): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
     }
 }
